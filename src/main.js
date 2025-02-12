bf3ec8c428458883310cfee7dfefc1902def61a7
fajarikas <fajarika.2021@student.uny.ac.id>
2025-02-12 08:54:56 +0700

    import css at main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
+import "./css/colors.css";
+import "./css/darkMode.css";
+import "./css/plugins.css";
+import "./css/style.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
